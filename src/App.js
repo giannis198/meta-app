4da9760340425d394415e21bdf1d64c422d55b7e
giannis198 <john_nik@windowslive.com>
2023-11-30 10:25:54 +0200

    3: commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+import "./App.css";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
